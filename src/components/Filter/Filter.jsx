dd1d3cb571fb8969d9ebf54c1e4ab9075c96d1b4
Artem-Borzov <abug990@gmail.com>
2023-03-20 19:10:14 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from 'redux/filterSlice';
 
 const Filter = () => {
-  const filter = useSelector(state => state.phonebook.filter);
+  const filter = useSelector(state => state.filter.filter);
   const dispatch = useDispatch();
 
   const changeFilter = e => {
